09440fcfab9735d4083974d68836752212cd32f0
Not_Black <not_black_233@163.com>
2026-06-03 15:07:29 +0800

    Update PlayerJoinListener Subscribe `LoginEvent`

--- src/main/kotlin/me/not_black/whitelisted/listener/PlayerJoinListener.kt
+++ src/main/kotlin/me/not_black/whitelisted/listener/PlayerJoinListener.kt
@@ -1,7 +1,7 @@
 package me.not_black.whitelisted.listener
 
 import com.velocitypowered.api.event.Subscribe
-import com.velocitypowered.api.event.player.ServerPreConnectEvent
+import com.velocitypowered.api.event.connection.LoginEvent
 import me.not_black.whitelisted.Whitelisted
 import me.not_black.whitelisted.api.WhitelistAPI
 import net.kyori.adventure.text.Component
@@ -9,7 +9,7 @@ import kotlin.uuid.toKotlinUuid
 
 object PlayerJoinListener {
     @Subscribe
-    fun onServerPreConnectEvent(event: ServerPreConnectEvent) {
+    fun onServerPreConnectEvent(event: LoginEvent) {
         if (!Whitelisted.inst.config.enabled) return
         if (!WhitelistAPI.inWhitelist(event.player.uniqueId.toKotlinUuid()))
             event.player.disconnect(Component.text("You are not whitelisted!"))
